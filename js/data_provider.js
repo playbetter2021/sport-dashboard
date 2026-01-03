3b0f45ac2ecb305af28fa1efbd4b8f0ada7fbac6
playbetter2021 <playbetter2021@gmail.com>
2026-01-03 11:33:57 +0100

    Update data_provider.js

--- js/data_provider.js
+++ js/data_provider.js
@@ -1,14 +1,21 @@
 const DataProvider = {
     async getAllPlayers() {
         try {
+            // Timestamp voorkomt dat de browser de oude versie van de JSON laat zien
             const timestamp = new Date().getTime();
             const response = await fetch(`data/players.json?t=${timestamp}`);
-            if (!response.ok) return [];
+            
+            if (!response.ok) {
+                console.error("Geen verbinding met players.json");
+                return [];
+            }
+            
             return await response.json();
         } catch (error) {
-            console.error(error);
+            console.error("Kritieke fout:", error);
             return [];
         }
     }
 };
+
 export default DataProvider;
